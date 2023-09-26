77c369a95edf2ea260aba5624b01540f3bd1c7a6
AlyssaPng <e0959598@u.nus.edu>
2023-09-27 00:47:34 +0800

    Update Code Style

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,15 +9,33 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a new Remark instance.
+     *
+     * @param remark The string representation of Remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
     }
 
+    /**
+     * Overrides toString method.
+     * Returns the String representations of the Remark.
+     *
+     * @return the String representations of the Remark.
+     */
     @Override
     public String toString() {
         return value;
     }
+
+    /**
+     * Overrides equals method.
+     *
+     * @param other the Object to be compared with.
+     * @return True if both Objects are equal.
+     */
 
     @Override
     public boolean equals(Object other) {
@@ -26,6 +44,11 @@ public class Remark {
                 && value.equals(((Remark) other).value)); // state check
     }
 
+    /**
+     * Overrides hasCode method from parent class.
+     *
+     * @return the value hashCode.
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
